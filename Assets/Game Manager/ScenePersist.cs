add20854a8c07736e75f2fca9a1f68798d8155d9
nilo <nilold@gmail.com>
2018-11-11 20:00:23 -0200

    persisting scene between deaths

---
+++ Assets/Game Manager/ScenePersist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+
+    int startingSceneIndex;
+
+    private void Awake()
+    {
+        int objectCount = FindObjectsOfType<ScenePersist>().Length;
+        if(objectCount > 1){
+            Destroy(gameObject);
+        } else{
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start () {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int currentScenaIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentScenaIndex != startingSceneIndex)
+            Destroy(gameObject);
+	}
+}
