0167ed7ac1cd7bfcdef267be0086d33ce70cb0f6
nilo <nilold@gmail.com>
2018-11-10 23:00:02 -0200

    enemy movement

---
+++ Assets/Enemies/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidBody;
+    BoxCollider2D myCollider;
+
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	void Update () {
+        myRigidBody.velocity = new Vector2(moveSpeed, 0);
+	}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground"){
+            // flip
+            moveSpeed *= -1f;
+            transform.localScale = new Vector2(Mathf.Sign(moveSpeed), 1);
+        }
+    }
+
+}
